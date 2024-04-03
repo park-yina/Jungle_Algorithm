9c49ed5c72d9c0c0d49effc18cbee12899ea7599
park-yina <kln99988@gamil.com>
2024-04-03 10:32:17 +0900

    변수변경

--- 2665.py
+++ 2665.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 r, c = map(int, input().split())
 
 graph = [list(input().strip()) for _ in range(r)]
-dist = [[0] *c for _ in range(r)]
+visit = [[0] *c for _ in range(r)]
 dx= [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 queue = collections.deque()
@@ -14,14 +14,14 @@ def bfs(s,e):
     while queue:
         x,y = queue.popleft()
         if graph[s][e] == 'S':
-            return dist[s][e]
+            return visit[s][e]
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < r and 0 <= ny < c:
                 if (graph[nx][ny] == '.' or graph[nx][ny] == 'D') and graph[x][y] == 'S':
                     graph[nx][ny] = 'S'
-                    dist[nx][ny] = dist[x][y] + 1
+                    visit[nx][ny] = visit[x][y] + 1
                     queue.append((nx,ny))
                 elif (graph[nx][ny] =='.' or graph[nx][ny] =='S') and graph[x][y] == '*':
                     #고슴도치처럼 물도 이동처리 해주어야한다.
